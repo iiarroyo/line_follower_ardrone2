1d78674bca68e66328880f19e2ba12674453310e
israel-iap <israel.iap@outlookc.com>
2021-01-22 00:44:17 -0600

    functional square path

--- scripts/land.py
+++ scripts/land.py
@@ -6,26 +6,41 @@ from std_msgs.msg import Empty
 # to do:
 # agregar y crear topico, para determinar si se debe aterrizar
 
+def callback(data):
+    if(data.data):
+        takeoff(True)
+        land()
+        # rospy.on_shutdown(bye)
+def bye():
+    print("bye")
+
 def listen():
-    rospy.init_node("land",anonymous=True)
+    # rospy.init_node("land",anonymous=True)
     rospy.Subscriber("status",Bool,callback)
-    while not rospy.is_shutdown():
-      pub.publish(Empty())
-      rate.sleep()
+    # rospy.spin()
 
 
 def land():
     pub = rospy.Publisher("ardrone/land", Empty, queue_size=10 )
-    rospy.init_node('land', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
       pub.publish(Empty())
       rate.sleep()
 
+def takeoff():
+
+    pub = rospy.Publisher("ardrone/takeoff", Empty, queue_size=10 )
+    rospy.init_node("land",anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+
+    while not rospy.is_shutdown():
+        pub.publish(Empty())
+        listen()
+        rate.sleep()
+
 
 if __name__ == '__main__':
   try:
-   listen()
-   land()
+   takeoff()
   except rospy.ROSInterruptException:
    pass
