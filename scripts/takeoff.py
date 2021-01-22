f335283077e138ba5f42d65c8eb16938db8ea08c
israel-iap <israel.iap@outlookc.com>
2021-01-21 23:46:28 -0600

    implemented topic(square.py) and cleaned takeoff.py

--- scripts/takeoff.py
+++ scripts/takeoff.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 from std_msgs.msg import Empty
-# to do:
-# agregar y crear topico, para determinar si se debe aterrizar
 
 def takeoff():
-  pub = rospy.Publisher("ardrone/takeoff", Empty, queue_size=10 )
-  rospy.init_node('takeoff', anonymous=True)
-  rate = rospy.Rate(10) # 10hz
-  while not rospy.is_shutdown():
-    pub.publish(Empty())
-    rate.sleep()
+    pub = rospy.Publisher("ardrone/takeoff", Empty, queue_size=10 )
+    rospy.init_node('takeoff', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        pub.publish(Empty())
+        rate.sleep()
 
 
 if __name__ == '__main__':
