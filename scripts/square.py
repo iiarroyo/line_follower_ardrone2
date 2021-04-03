a330bbc15fffc677367d2fc870b160455eab3e68
israel-iap <israel.iap@outlookc.com>
2021-04-03 12:46:29 -0600

    added cvBridge error handle

--- scripts/square.py
+++ scripts/square.py
@@ -2,10 +2,12 @@
 
 # Company : ITESM - Campus Qro
 # Author : Israel Ivan Arroyo P A01706190
-# Project name : movement.py
+# Project name : square.py
 # target device: AR parrot drone 2, 2.3.3 frimware version
 # target simulation: gazebo v9.0.0 with tum_simulator_meodic ros library
 # tool version : python 2.7.17
+# Description: Make drone follow a square pattern.
+
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Empty
@@ -17,7 +19,6 @@ import numpy as np
 class Quadrotor:
 
     def __init__(self):
-        self.breakFlag=False
         self.cmdPub = rospy.Publisher("cmd_vel",Twist, queue_size=10)
         self.landPub = rospy.Publisher("ardrone/land", Empty, queue_size=10 )
         self.takeoffPub = rospy.Publisher("ardrone/takeoff", Empty, queue_size=10)
@@ -45,7 +46,8 @@ class Quadrotor:
 
     def takeoff(self):
         # despegar
-        while (not rospy.is_shutdown()) and (self.navdataVar.altd < 800):
+        while (not rospy.is_shutdown()):# and (self.navdataVar.state != 3):
+            # print(self.navdataVar.altd)
             self.takeoffPub.publish(Empty())
             self.rate.sleep()
 
@@ -65,7 +67,7 @@ class Quadrotor:
     def turn_right(self):
         # dar vuelta hasta limite
         vel = Twist()
-        vel.angular.z=-0.5
+        vel.angular.z=-1.0
         vel.linear.x=0.0
         rad = 0.0
 
@@ -82,7 +84,7 @@ class Quadrotor:
     def go_straight(self,pos):
         # ir derecho hasta limite
         vel = Twist()
-        vel.linear.x = 0.1
+        vel.linear.x = 1.0
         vel.angular.z = 0.0
         # vel.linear.z=0.3
 
@@ -115,43 +117,4 @@ if __name__ == '__main__':
       quad.land()
   except rospy.ROSInterruptException:
       pass
-# Reference : https://ardrone-autonomy.readthedocs.io/en/latest/reading.html
-# header:
-#   seq: 4785
-#   stamp:
-#     secs: 23
-#     nsecs: 930000000
-#   frame_id: "ardrone_base_link"
-# batteryPercent: 99.1004180908
-# state: 3
-# magX: 0
-# magY: 0
-# magZ: 0
-# pressure: 0
-# temp: 0
-# wind_speed: 0.0
-# wind_angle: 0.0
-# wind_comp_angle: 0.0
-# rotX: 0.0520852431655
-# rotY: -0.0970923304558
-# rotZ: -0.670082449913
-# altd: 599
-# vx: 951.08013916
-# vy: 3.64785599709
-# vz: 11.7337741852
-# ax: -0.00248119980097
-# ay: 0.000215476015001
-# az: 1.00660967827
-# motor1: 0
-# motor2: 0
-# motor3: 0
-# motor4: 0
-# tags_count: 0
-# tags_type: []
-# tags_xc: []
-# tags_yc: []
-# tags_width: []
-# tags_height: []
-# tags_orientation: []
-# tags_distance: []
-# tm: 23930000.0
+# Reference for navdata type: https://ardrone-autonomy.readthedocs.io/en/latest/reading.html
